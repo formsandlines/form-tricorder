39c99bcd95aceb929f7130fb8c65d4c448f05acc
Peter Hofmann <peter.hofmann@formsandlines.eu>
2023-03-14 13:41:41 +0100

    Decided to try a different approach to application design by incremental growth and iteration as opposed to top-down design and filling in the details. My initial impulse was to just implement what I have designed on paper, but I am curious to see if I can build it in incremental steps that focus on essential parts of the application first without trying to add all the functionality up front. The idea is to start simple and then grow on top of a working prototype that already provides value to a potential user. This way, I hope to think more clearly about the design and build a more sustainable architecture while avoiding unnecessary complexity. Maybe I’ll roll back to where I was, which was not bad, but this is just a personal experiment.

--- src/cljs/form_tricorder/core.cljs
+++ src/cljs/form_tricorder/core.cljs
@@ -1,11 +1,5 @@
 (ns form-tricorder.core
   (:require
-   [form-tricorder.processor :refer [process]]
-   [form-tricorder.contexts :refer [OutputContext]]
-   [form-tricorder.components.app-toolbar :refer [AppToolbar]]
-   [form-tricorder.components.input-area :refer [InputArea]]
-   [form-tricorder.components.function-menubar :refer [FunctionMenubar]]
-   [form-tricorder.components.output-area :refer [OutputArea]]
    [helix.core :refer [defnc $ <> provider]]
    [helix.hooks :as hooks]
    [helix.dom :as d]
@@ -15,32 +9,7 @@
 
 (defnc App
   []
-  (let [[input set-input] (hooks/use-state "((a)b)")
-        [views set-views] (hooks/use-state
-                           [{:mode-id "calc", :func-id "vtable",
-                             :active true}
-                            {:mode-id "visu", :func-id "hooks",
-                             :active true}])
-        process-view (partial process input)
-        style (-> {:backgroundColor "darkgray"}
-                  clj->js
-                  css)]
-    (d/div
-     {:class (style)}
-     ($ AppToolbar
-        {:views     views
-         :set-views set-views})
-     ($ InputArea
-        {:submit-handler set-input})
-     ($ FunctionMenubar
-        {:views     views
-         :set-views set-views})
-     (provider
-      {:context OutputContext
-       :value   process-view}
-      ($ OutputArea
-         {:views     views
-          :set-views set-views})))))
+  (d/h1 "FORM tricorder"))
 
 
 (defonce root
